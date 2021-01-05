174c8c1c73fb3dcca0695a28a8d710199f21572c
Shana Wiltshire <shanawiltshire@gmail.com>
2021-01-05 00:17:54 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-// Code your solutions in this file
+const names = ["Ada", "Brendan", "Ali"];
+
+function writeCards(names, event) {
+  let birthday =[];
+  event = "surprise";
+  for (let i= 0; i < names.length; i++) {
+  birthday[i] = 'Thank you, '+ names[i] +', for the wonderful ' +event+' gift!';
+  }
+  return birthday;
+}
+
+let countup = 0;
+while (countup < 10) {
+  console.log(countup++);
+}
